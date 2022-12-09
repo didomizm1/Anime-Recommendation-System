15f0738389b67692c017f0e4c2d79271777423b0
didomizm1 <didomizm1@newpaltz.edu>
2022-12-09 01:50:42 -0500

    Import Centroids Fixed

--- code/py/map_reduce/mapper.py
+++ code/py/map_reduce/mapper.py
@@ -6,60 +6,75 @@ import sys
 
 from math import sqrt
 
-# Choose initial centroids
-def initial_centroids(filepath):
-    centroids=[]
-    with open(filepath) as fp:
-        line=fp.readline()
-        while line:
-            if line:
-                try:
-                    line=line.strip()
-                    coordinate=line.split(',')
-                    for x in range(42):
-                        centroids.append([float(coordinate[x)])
-                except:
-                    break
-            else:
-                break
-            line=fp.readline()
-    fp.close()
-    return centroids
 
-# Generate clusters based on chosen centroids
-def generate_clusters(centroids):
-    for line in sys.stdin:
-        line=line.strip()
-        coordinate=line.split(',')
-        min_distance=10000000000
-        index=0
-        #Keeps track of the centroid number for calculating the distances
-        centroid_number=0
-        for centroid in centroids:
-            try:
-                for x in range(42):
-                    coordinate[x]=float(coordinate[x])
-            except ValueError:
-                continue
-            # Euclidian distance, calculates the distance between the point and each centroid
-            distance=0
+# Import centroids, either initial centroids on first iteration or centroids calculated by reducer on further iterations
+def import_centroids(filepath):
+    # Open file and create list of centroids
+    centroids_list = []
+    with open(filepath, "r") as file:
+        # Skip first line
+        next(file)
+
+        # Iterate through each line of centroids file
+        for line in file:
+
+            centroid = []  # Centroid to be added to centroids list
+            coordinates = line.split(',')  # Centroid coordinates before processing
+
+            # Iterate through coordinates of a centroid
             for x in range(42):
-                if np.isnan(coordinate[x]) #Checking if coordinate is a NaN value
-                    distance=distance
-                else
-                    distance+=(pow(coordinate[x]-centroid[centroid_number],2))
+                # If coordinate is not an NaN value, add to centroid as a float, otherwise add NaN value itself
+                current_coordinate = coordinates[x]
+                try:
+                    centroid.append(float(current_coordinate))
+                except (Exception,):
+                    centroid.append(current_coordinate)
 
-            current_distance=sqrt(distance)
-            # Find the centroid closer to the point
-            if current_distance <= min_distance:
-                min_distance=current_distance
-                index=centroids.index(centroid)
-            centroid_number+=1
-        print(index)
+            # Add processed centroid to centroids list
+            centroids_list.append(centroid)
+
+    # Close file, then return list of centroids
+    file.close()
+
+    return centroids_list
+
+
+# Generate clusters based on centroids
+def generate_clusters(centroids_list):
+    
+    index = 0
+
+    # Keeps track of the centroid number for calculating the distances
+    centroid_number = 0
+    for centroid in centroids_list:
+        try:
+            for x in range(42):
+                coordinate[x] = float(coordinate[x])
+        except ValueError:
+            continue
+
+        # Euclidian distance, calculates the distance between the point and each centroid
+        distance = 0
         for x in range(42):
-            print(", " + coordinate[x])
-            
+            if np.isnan(coordinate[x]):  # Checking if coordinate is a NaN value
+                distance = distance
+            else:
+                distance += (pow(coordinate[x]-centroid[centroid_number], 2))
+
+        current_distance = sqrt(distance)
+        # Find the centroid closer to the point
+        if current_distance <= min_distance:
+            min_distance = current_distance
+            index = centroids_list.index(centroid)
+        centroid_number += 1
+
+    print(index)
+
+    for x in range(42):
+        print(", " + coordinate[x])
+
+
 # Call functions in order to print data for reducer
 if __name__ == '__main__':
-    centroids=initial_centroids('../../../data/centroids/initial_centroids.csv') 
+    centroids = import_centroids('../../../data/centroids/centroids.csv')
     generate_clusters(centroids)
